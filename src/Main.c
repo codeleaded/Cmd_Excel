fe1d7bbdd54f3a5ea9c284e5756ab73503e485ec
codeleaded <codeleaded@gmail.com>
2025-10-18 16:43:01 +0200

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,8 +1,21 @@
-//#include "/home/codeleaded/System/Static/Container/Deque.h"
+#if defined(__linux__) && !defined(_WIN32)
+    #include "/home/codeleaded/System/Static/Library/Excel.h"
+#elif defined(_WIN32) || defined(_WIN64)
+    #include "/home/codeleaded/System/Static/Library/Excel.h"
+#elif defined(__APPLE__)
+    #error "Apple not supported!"
+#else
+    #error "Platform not supported!"
+#endif
+
 
 int main(int argc,const char *argv[]){
     
+    Excel e = Excel_New(10,10);
+
     
+
+    Excel_Free(&e);
 
     return 0;
 }
