6acdbbc8f59c3f8fe9361477cc029a561339317e
C-Reaper <gnu.c.reaper@gmail.com>
2025-10-18 23:55:03 +0200

    updated

--- libs/vbcell.c
+++ libs/vbcell.c
@@ -4,14 +4,14 @@
 
 void VBCell_Destroyer(Variable* v){
     //printf("VBCell: Destroyer!\n");
-    CStr* str = (CStr*)Variable_Data(v);
-    CStr_Free(str);
+    //CStr* str = (CStr*)Variable_Data(v);
+    //CStr_Free(str);
 }
 void VBCell_Cpyer(Variable* src,Variable* dst){
     //printf("VBCell: Cpyer!\n");
-    CStr* src_str = (CStr*)Variable_Data(src);
-    CStr* dst_str = (CStr*)Variable_Data(dst);
-    *dst_str = CStr_Cpy(*src_str);
+    //CStr* src_str = (CStr*)Variable_Data(src);
+    //CStr* dst_str = (CStr*)Variable_Data(dst);
+    //*dst_str = CStr_Cpy(*src_str);
 }
 
 Token VBCell_VBCell_Handler_Ass(Scope* s,Token* op,Vector* args){
@@ -34,53 +34,24 @@ Token VBCell_VBCell_Handler_Ass(Scope* s,Token* op,Vector* args){
     }
     */
 
-    return Token_By(TOKEN_CONSTSTRING_DOUBLE,NULL);
+    return Token_Move(TOKEN_NONE,NULL);
 }
 Token VBCell_VBCell_Handler_Add(Scope* s,Token* op,Vector* args){
     Token* a = (Token*)Vector_Get(args,0);
     Token* b = (Token*)Vector_Get(args,1);
-
-    //printf("ADD: %s + %s\n",a->str,b->str);
-
-    CStr n1 = Implementation_VBCellOf(s,a);
-    CStr n2 = Implementation_VBCellOf(s,b);
-    int s1 = CStr_Size(n1);
-    int s2 = CStr_Size(n2);
-    
-    CStr res = malloc(s1+s2+1);
-    memcpy(res,n1,s1);
-    memcpy(res+s1,n2,s2+1);
-
-    return Token_Move(TOKEN_CONSTSTRING_DOUBLE,res);
+    return Token_Move(TOKEN_NONE,NULL);
 }
 Token VBCell_VBCell_Handler_Equ(Scope* s,Token* op,Vector* args){
     Token* a = (Token*)Vector_Get(args,0);
     Token* b = (Token*)Vector_Get(args,1);
-
-    //printf("EQU: %s == %s\n",a->str,b->str);
-
-    CStr n1 = Implementation_VBCellOf(s,a);
-    CStr n2 = Implementation_VBCellOf(s,b);
-    Boolean res = CStr_Cmp(n1,n2);
-
-    char* resstr = Boolean_Get(res);
-    return Token_Move(TOKEN_BOOL,resstr);
+    return Token_Move(TOKEN_NONE,NULL);
 }
 Token VBCell_Handler_Cast(Scope* s,Token* op,Vector* args){
     Token* a = (Token*)Vector_Get(args,0);
-
-    //printf("CAST: %s\n",a->str);
-
-    CStr n1 = Implementation_VBCellOf(s,a);
-    
-    CStr res = n1;
-    char* resstr = CStr_Cpy(res);
-    return Token_Move(TOKEN_CONSTSTRING_DOUBLE,resstr);
+    return Token_Move(TOKEN_CONSTSTRING_DOUBLE,NULL);
 }
 Token VBCell_Handler_Destroy(Scope* s,Token* op,Vector* args){
     Token* a = (Token*)Vector_Get(args,0);
-
-    //printf("DESTROY: %s\n",a->str);
 
     Variable* a_var = Scope_FindVariable(s,a->str);
     if(a_var){
@@ -92,14 +63,14 @@ Token VBCell_Handler_Destroy(Scope* s,Token* op,Vector* args){
 
 void Ex_Packer(ExternFunctionMap* Extern_Functions,Vector* funcs,Scope* s){//Vector<CStr>
     TypeMap_PushContained(&s->types,funcs,
-        Type_New("str",8,OperatorInterationMap_Make((OperatorInterater[]){
+        Type_New("vbcell",sizeof(void*),OperatorInterationMap_Make((OperatorInterater[]){
             OperatorInterater_Make((CStr[]){ NULL },OperatorDefineMap_Make((OperatorDefiner[]){
                 OperatorDefiner_New(TOKEN_CAST,(Token(*)(void*,Token*,Vector*))VBCell_Handler_Cast),
                 OperatorDefiner_New(TOKEN_INIT,NULL),
                 OperatorDefiner_New(TOKEN_DESTROY,NULL),
                 OPERATORDEFINER_END
             })),
-            OperatorInterater_Make((CStr[]){ "str",NULL },OperatorDefineMap_Make((OperatorDefiner[]){
+            OperatorInterater_Make((CStr[]){ "vbcell",NULL },OperatorDefineMap_Make((OperatorDefiner[]){
                 OperatorDefiner_New(TOKEN_VBLIKE_ASS,(Token(*)(void*,Token*,Vector*))VBCell_VBCell_Handler_Ass),
                 OperatorDefiner_New(TOKEN_VBLIKE_ADD,(Token(*)(void*,Token*,Vector*))VBCell_VBCell_Handler_Add),
                 OperatorDefiner_New(TOKEN_VBLIKE_EQU,(Token(*)(void*,Token*,Vector*))VBCell_VBCell_Handler_Equ),
--- src/Main.c
+++ src/Main.c
@@ -11,18 +11,20 @@
 
 int main(int argc,const char *argv[]){
     
-    Excel e = Excel_New(10,10);
+    Excel e = Excel_New("./bin",10,10);
 
-    Excel_Set(&e,0,0,"= 1 + 2");
-    Excel_Set(&e,1,0,"= 3 + 4");
-    Excel_Set(&e,2,0,"= $(0,0) + $(1,0)");
+    // Excel_Set(&e,0,0,"= 1 + 2");
+    // Excel_Set(&e,1,0,"= 3 + 4");
+    // Excel_Set(&e,2,0,"= $(0,0) + $(1,0)");
 
-    Excel_Script(&e.vbl.ev,"./code/Main.vbl");
+    // Excel_Script(&e.vbl.ev,"./code/Main.vbl");
     
-    Variable ret = Excel_Function(&e.vbl.ev,"main",(Variable[]){
-        Variable_Null()
-    });
-    Variable_Free(&ret);
+    // Variable ret = Excel_Function(&e.vbl.ev,"main",(Variable[]){
+    //     Variable_Null()
+    // });
+    // Variable_Free(&ret);
+
+    Excel_Print(&e);
 
     Excel_Free(&e);
 
