40f47ef015b70c7875abb61e5c45b316210cd805
C-Reaper <gnu.c.reaper@gmail.com>
2025-10-20 23:50:17 +0200

    updated

--- libs/vbcell.c
+++ libs/vbcell.c
@@ -2,122 +2,17 @@
 #include "/home/codeleaded/System/Static/Library/AlxExternFunctions.h"
 #include "/home/codeleaded/System/Static/Library/Excel.h"
 
-/*
-Token VBCell_Any_Handler_Ass(Excel* e,Token* op,Vector* args){
-    Token* a = (Token*)Vector_Get(args,0);
-    Token* b = (Token*)Vector_Get(args,1);
-
-    printf("ASS: %s = %s\n",a->str,b->str);
-
-    ExcelCell* vbcell_a = Excel_VBCell_Get(e,a);
-
-    if(b->tt == TOKEN_VBLIKE_CELL){
-        ExcelCell* vbcell_b = Excel_VBCell_Get(e,b);
-        if(vbcell_b){
-            vbcell_a->type = CStr_Cpy(vbcell_b);
-
-            const int size = TypeMap_SizeOf(&e->vbl.ev.sc.types,vbcell_b->type);
-            vbcell_a->output = malloc(size);
-            memcpy(vbcell_a->output,vbcell_b->output,size);
-        }
-    }else{
-        if(b->tt == TOKEN_STRING){
-            const Vic2 pos = VBLike_ExtractCoords(a->str);
-            Variable* v_b = Scope_FindVariable(&e->vbl.ev.sc,b->str);
-            
-            char buffer[32];
-            sprintf(buffer,"$(%d,%d)",pos.x,pos.y);
-            Variable v_vbcell = Variable_New(buffer,NULL,-1,-1,NULL,NULL);
-
-            Variable_Set(&v_vbcell,v_b);
-
-            vbcell_a->output = v_vbcell.data;
-            vbcell_a->type = v_vbcell.typename;
-            v_vbcell.data = NULL;
-            v_vbcell.typename = NULL;
-
-            Variable_Free(&v_vbcell);
-        }else if(b->tt == TOKEN_NUMBER){
-            vbcell_a->output = malloc(sizeof(I64));
-            *(I64*)vbcell_a->output = Excel_Int_Get(e,b);
-            CStr_Set(&vbcell_a->type,"int");
-        }else if(b->tt == TOKEN_FLOAT){
-            vbcell_a->output = malloc(sizeof(F64));
-            *(F64*)vbcell_a->output = Excel_Float_Get(e,b);
-            CStr_Set(&vbcell_a->type,"float");
-        }else if(b->tt == TOKEN_BOOL){
-            vbcell_a->output = malloc(sizeof(Boolean));
-            *(Boolean*)vbcell_a->output = Excel_Bool_Get(e,b);
-            CStr_Set(&vbcell_a->type,"bool");
-        }else if(b->tt == TOKEN_CONSTSTRING_DOUBLE){
-            vbcell_a->output = malloc(sizeof(CStr));
-            *(CStr*)vbcell_a->output = CStr_Cpy(Excel_Str_Get(e,b));
-            CStr_Set(&vbcell_a->type,"str");
-        }else if(b->tt == TOKEN_FUNCTIONPOINTER){
-            vbcell_a->output = malloc(sizeof(CStr));
-            *(CStr*)vbcell_a->output = CStr_Cpy(Excel_Str_Get(e,b));
-            CStr_Set(&vbcell_a->type,"func");
-        }
-    }
-
-    return Token_Cpy(a);
+void VBCell_Destroyer(Variable* v){
+    //printf("VBCell: Destroyer!\n");
+    CStr* str = (CStr*)Variable_Data(v);
+    CStr_Free(str);
 }
-Token VBCell_Any_Handler_Add(Excel* e,Token* op,Vector* args){
-    Token* a = (Token*)Vector_Get(args,0);
-    Token* b = (Token*)Vector_Get(args,1);
-
-    printf("ADD: %s + %s\n",a->str,b->str);
-
-    ExcelCell* vbcell_a = Excel_VBCell_Get(e,a);
-
-    if(b->tt == TOKEN_VBLIKE_CELL){
-        ExcelCell* vbcell_b = Excel_VBCell_Get(e,b);
-        if(vbcell_b){
-            vbcell_a->output,vbcell_b->output
-        }
-    }else{
-        if(b->tt == TOKEN_STRING){
-            const Vic2 pos = VBLike_ExtractCoords(a->str);
-            Variable* v_b = Scope_FindVariable(&e->vbl.ev.sc,b->str);
-            
-            char buffer[32];
-            sprintf(buffer,"$(%d,%d)",pos.x,pos.y);
-            Variable v_vbcell = Variable_New(buffer,NULL,-1,-1,NULL,NULL);
-
-            Variable_Set(&v_vbcell,v_b);
-
-            vbcell_a->output = v_vbcell.data;
-            vbcell_a->type = v_vbcell.typename;
-            v_vbcell.data = NULL;
-            v_vbcell.typename = NULL;
-
-            Variable_Free(&v_vbcell);
-        }else if(b->tt == TOKEN_NUMBER){
-            vbcell_a->output = malloc(sizeof(I64));
-            *(I64*)vbcell_a->output = Excel_Int_Get(e,b);
-            CStr_Set(&vbcell_a->type,"int");
-        }else if(b->tt == TOKEN_FLOAT){
-            vbcell_a->output = malloc(sizeof(F64));
-            *(F64*)vbcell_a->output = Excel_Float_Get(e,b);
-            CStr_Set(&vbcell_a->type,"float");
-        }else if(b->tt == TOKEN_BOOL){
-            vbcell_a->output = malloc(sizeof(Boolean));
-            *(Boolean*)vbcell_a->output = Excel_Bool_Get(e,b);
-            CStr_Set(&vbcell_a->type,"bool");
-        }else if(b->tt == TOKEN_CONSTSTRING_DOUBLE){
-            vbcell_a->output = malloc(sizeof(CStr));
-            *(CStr*)vbcell_a->output = CStr_Cpy(Excel_Str_Get(e,b));
-            CStr_Set(&vbcell_a->type,"str");
-        }else if(b->tt == TOKEN_FUNCTIONPOINTER){
-            vbcell_a->output = malloc(sizeof(CStr));
-            *(CStr*)vbcell_a->output = CStr_Cpy(Excel_Str_Get(e,b));
-            CStr_Set(&vbcell_a->type,"func");
-        }
-    }
-
-    return Token_Move(TOKEN_NUMBER,NULL);
+void VBCell_Cpyer(Variable* src,Variable* dst){
+    //printf("VBCell: Cpyer!\n");
+    CStr* src_str = (CStr*)Variable_Data(src);
+    CStr* dst_str = (CStr*)Variable_Data(dst);
+    *dst_str = CStr_Cpy(*src_str);
 }
-*/
 
 Token VBCell_VBCell_Handler_Ass(Excel* e,Token* op,Vector* args){
     Token* a = (Token*)Vector_Get(args,0);
@@ -127,36 +22,56 @@ Token VBCell_VBCell_Handler_Ass(Excel* e,Token* op,Vector* args){
 
     ExcelCell* ec = Excel_VBCell_Get(e,a);
 
-    if(CStr_Cmp(ec->type,"int")){
-        if(ec->output) free(ec->output);
-        CStr_Set(&ec->type,"int");
-        ec->output = malloc(sizeof(Number));
-        *(Number*)ec->output = Excel_Int_Get(e,b);
-    }else if(CStr_Cmp(ec->type,"float")){
-        if(ec->output) free(ec->output);
-        CStr_Set(&ec->type,"float");
-        ec->output = malloc(sizeof(Double));
-        *(Double*)ec->output = Excel_Float_Get(e,b);
-    }else if(CStr_Cmp(ec->type,"bool")){
-        if(ec->output) free(ec->output);
-        CStr_Set(&ec->type,"bool");
-        ec->output = malloc(sizeof(Boolean));
-        *(Boolean*)ec->output = Excel_Bool_Get(e,b);
-    }else if(CStr_Cmp(ec->type,"str")){
-        if(ec->output) free(ec->output);
-        CStr_Set(&ec->type,"str");
-        ec->output = CStr_Cpy(Excel_Str_Get(e,b));
-    }else if(CStr_Cmp(ec->type,"func")){
-        if(ec->output) free(ec->output);
-        CStr_Set(&ec->type,"func");
-        ec->output = CStr_Cpy(Excel_Str_Get(e,b));
-    }else{
-        if(ec->output) free(ec->output);
-        CStr_Free(&ec->type);
-        ec->type = NULL;
+    if(ec){
+        if(CStr_Cmp(ec->type,"int")){
+            if(ec->output) free(ec->output);
+            CStr_Set(&ec->type,"int");
+            ec->output = malloc(sizeof(Number));
+            *(Number*)ec->output = Excel_Int_Get(e,b);
+        }else if(CStr_Cmp(ec->type,"float")){
+            if(ec->output) free(ec->output);
+            CStr_Set(&ec->type,"float");
+            ec->output = malloc(sizeof(Double));
+            *(Double*)ec->output = Excel_Float_Get(e,b);
+        }else if(CStr_Cmp(ec->type,"bool")){
+            if(ec->output) free(ec->output);
+            CStr_Set(&ec->type,"bool");
+            ec->output = malloc(sizeof(Boolean));
+            *(Boolean*)ec->output = Excel_Bool_Get(e,b);
+        }else if(CStr_Cmp(ec->type,"str")){
+            if(ec->output) free(ec->output);
+            CStr_Set(&ec->type,"str");
+            ec->output = CStr_Cpy(Excel_Str_Get(e,b));
+        }else if(CStr_Cmp(ec->type,"func")){
+            if(ec->output) free(ec->output);
+            CStr_Set(&ec->type,"func");
+            ec->output = CStr_Cpy(Excel_Str_Get(e,b));
+        }else{
+            if(ec->output) free(ec->output);
+            CStr_Free(&ec->type);
+            ec->type = NULL;
 
-        ExcelCell* ec_b = Excel_VBCell_Get(e,b);
-        ec->output = CStr_Cpy((CStr)ec_b->output);
+            ExcelCell* ec_b = Excel_VBCell_Get(e,b);
+            ec->output = CStr_Cpy((CStr)ec_b->output);
+        }
+    }else if(a->tt==TOKEN_STRING){
+        Variable* a_var = Scope_FindVariable(&e->vbl.ev.sc,a->str);
+        const Vic2 pos = VBLike_ExtractCoords(&e->vbl,a);
+        
+        if(a_var){
+            Variable_PrepairFor(a_var,sizeof(CStr),"vbcell",VBCell_Destroyer,VBCell_Cpyer);
+            Variable_SetTo(a_var,(CStr[]){ CStr_Format("%d,%d",pos.x,pos.y) });
+        }else{
+            Scope_BuildInitVariableRange(
+                &e->vbl.ev.sc,
+                a->str,
+                "vbcell",
+                e->vbl.ev.sc.range - 1,
+                (CStr[]){ CStr_Format("%d,%d",pos.x,pos.y) }
+            );
+        }
+    }else{
+        Interpreter_ErrorHandler(&e->vbl.ev,"[VBCell_VBCell_Ass] Operator %s undefined for: %s and %s!\n",op->str,a->str,b->str);
     }
 
     return Token_Cpy(a);
@@ -189,7 +104,7 @@ Token VBCell_Int_Handler_Ass(Excel* e,Token* op,Vector* args){
 
     printf("ASS: %s = %s\n",a->str,b->str);
 
-    const Vic2 pos = VBLike_ExtractCoords(a->str);
+    //const Vic2 pos = VBLike_ExtractCoords(&e->vbl,a);
     ExcelCell* vbcell_a = Excel_VBCell_Get(e,a);
 
     if(vbcell_a->output) free(vbcell_a->output);
@@ -214,7 +129,7 @@ Token VBCell_Str_Handler_Ass(Excel* e,Token* op,Vector* args){
 
     printf("ASS: %s = %s\n",a->str,b->str);
 
-    const Vic2 pos = VBLike_ExtractCoords(a->str);
+    //const Vic2 pos = VBLike_ExtractCoords(&e->vbl,a);
     ExcelCell* vbcell_a = Excel_VBCell_Get(e,a);
 
     if(vbcell_a->output) free(vbcell_a->output);
@@ -234,19 +149,18 @@ Token VBCell_Str_Handler_Add(Excel* e,Token* op,Vector* args){
     return Token_Move(TOKEN_CONSTSTRING_DOUBLE,CStr_Concat(Excel_Str_Get(e,a),Excel_Str_Get(e,b)));
 }
 
-
 Token VBCell_Handler_Cast(Excel* e,Token* op,Vector* args){
     Token* a = (Token*)Vector_Get(args,0);
 
     //ExcelCell* ec = Excel_VBCell_Get(e,a);
-    const Vic2 pos = VBLike_ExtractCoords(a->str);
+    const Vic2 pos = VBLike_ExtractCoords(&e->vbl,a);
 
     return Token_Move(TOKEN_CONSTSTRING_DOUBLE,CStr_Format("$(%d,%d)",pos.x,pos.y));
 }
 
 void Ex_Packer(ExternFunctionMap* Extern_Functions,Vector* funcs,Scope* s){//Vector<CStr>
     TypeMap_PushContained(&s->types,funcs,
-        Type_New("vbcell",sizeof(void*),OperatorInterationMap_Make((OperatorInterater[]){
+        Type_New("vbcell",sizeof(CStr),OperatorInterationMap_Make((OperatorInterater[]){
             OperatorInterater_Make((CStr[]){ NULL },OperatorDefineMap_Make((OperatorDefiner[]){
                 OperatorDefiner_New(TOKEN_CAST,(Token(*)(void*,Token*,Vector*))VBCell_Handler_Cast),
                 OPERATORDEFINER_END
